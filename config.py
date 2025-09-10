1c8e7a5ef70696a39c677ee31165e57ea1f58384
Ivavi17 <avi17803@gmail.com>
2025-09-10 18:21:48 +0530

    latets changes

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
-import os
-import json
+import os, json
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# Load credentials from environment variable
-GOOGLE_CREDENTIALS = json.loads(os.getenv('GOOGLE_CREDENTIALS', '{}'))
+GOOGLE_CREDENTIALS = json.loads(os.getenv("GOOGLE_CREDENTIALS", "{}"))
