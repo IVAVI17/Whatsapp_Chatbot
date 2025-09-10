040bc8bd5337142d7c3725ef2d4590a1906b6fc1
Ivavi17 <avi17803@gmail.com>
2025-09-10 18:39:38 +0530

    updated config.py

--- config.py
+++ config.py
@@ -1,6 +1,17 @@
-import os, json
+import os
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
 
-GOOGLE_CREDENTIALS = json.loads(os.getenv("GOOGLE_CREDENTIALS", "{}"))
+# Load credentials from env
+raw_creds = os.getenv("GOOGLE_CREDENTIALS", "{}")
+
+try:
+    GOOGLE_CREDENTIALS = json.loads(raw_creds)
+
+    # Fix private_key formatting (convert \\n → \n)
+    if "private_key" in GOOGLE_CREDENTIALS:
+        GOOGLE_CREDENTIALS["private_key"] = GOOGLE_CREDENTIALS["private_key"].replace("\\n", "\n")
+except json.JSONDecodeError:
+    GOOGLE_CREDENTIALS = {}
